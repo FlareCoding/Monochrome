37e51555baa7183b1920101460a96b339db6ba88
Anavaran <zyao2003@gmail.com>
2022-08-11 17:57:26 -0400

    Fully implemented the progress bar widget.

--- src/widgets/ProgressBar.cpp
+++ src/widgets/ProgressBar.cpp
@@ -1,40 +1,43 @@
 #include "ProgressBar.h"
 
 namespace mc {
-	ProgressBar::ProgressBar() {
-		_createVisuals();
-		_setupProperties();
-	}
+    ProgressBar::ProgressBar() {
+        _createVisuals();
+        _setupProperties();
+    }
 
-	Size ProgressBar::_measureSize() {
-		return Size(fixedWidth, fixedHeight);
-	}
+    Size ProgressBar::_measureSize() {
+        return Size(fixedWidth, fixedHeight);
+    }
 
-	void ProgressBar::_onSetComputedSize(const Size& size) {
-		float percentage = (float)progression / (maxValue - minValue);
-		d_progressVisual->customWidth = size.width * percentage;
-	}
+    void ProgressBar::_onSetComputedSize(const Size& size) {
+        float percentage =	static_cast<float>(value - minValue) /
+                            static_cast<float>(maxValue - minValue);
 
-	void ProgressBar::_createVisuals() {
-		d_bodyVisual = MakeRef<RectVisual>();
-		backgroundColor.forwardAssignment(&d_bodyVisual->color);
-		addCoreVisualElement(d_bodyVisual);
+        float progressWidth = size.width * percentage;
 
-		d_progressVisual = MakeRef<RectVisual>();
-		progressColor.forwardAssignment(&d_progressVisual->color);
-		addCoreVisualElement(d_progressVisual);
-	}
+        d_progressVisual->customWidth = static_cast<uint32_t>(progressWidth);
+    }
 
-	void ProgressBar::_setupProperties() {
-		fixedHeight = 20;
-		fixedWidth = 400;
-		minValue = 0;
-		maxValue = 100;
-		progression = 33;
-		backgroundColor = Color::gray;
-		progressColor = Color::red;
-		backgroundColor.forwardEmittedEvents(this);
-		progressColor.forwardEmittedEvents(this);
-	}
+    void ProgressBar::_createVisuals() {
+        d_bodyVisual = MakeRef<RectVisual>();
+        backgroundColor.forwardAssignment(&d_bodyVisual->color);
+        addCoreVisualElement(d_bodyVisual);
 
-}
+        d_progressVisual = MakeRef<RectVisual>();
+        progressColor.forwardAssignment(&d_progressVisual->color);
+        addCoreVisualElement(d_progressVisual);
+    }
+
+    void ProgressBar::_setupProperties() {
+        fixedHeight = 20;
+        fixedWidth = 220;
+        minValue = 0;
+        maxValue = 100;
+        value = 33;
+        backgroundColor = Color::gray;
+        progressColor = Color::blue;
+        backgroundColor.forwardEmittedEvents(this);
+        progressColor.forwardEmittedEvents(this);
+    }
+} // namespace mc
--- src/widgets/ProgressBar.h
+++ src/widgets/ProgressBar.h
@@ -4,25 +4,25 @@
 #include "visuals/BorderVisual.h"
 
 namespace mc {
-	class ProgressBar : public BaseWidget {
-	public:
-		ProgressBar();
+class ProgressBar : public BaseWidget {
+public:
+    ProgressBar();
 
-		PropertyObserver<Color>       backgroundColor;
-		PropertyObserver<Color>       progressColor;
-		PropertyObserver<uint32_t>    progression;
-		PropertyObserver<int32_t>     maxValue;
-		PropertyObserver<int32_t>     minValue;
+    PropertyObserver<Color>       backgroundColor;
+    PropertyObserver<Color>       progressColor;
+    PropertyObserver<int32_t>     value;
+    PropertyObserver<int32_t>     maxValue;
+    PropertyObserver<int32_t>     minValue;
 
-	protected:
-		Size _measureSize() override;
-		void _onSetComputedSize(const Size& size) override;
+protected:
+    Size _measureSize() override;
+    void _onSetComputedSize(const Size& size) override;
 
-	private:
-		void _createVisuals();
-		void _setupProperties();
+private:
+    void _createVisuals();
+    void _setupProperties();
 
-		Shared<RectVisual> d_bodyVisual;
-		Shared<RectVisual> d_progressVisual;
-	};
-}
+    Shared<RectVisual> d_bodyVisual;
+    Shared<RectVisual> d_progressVisual;
+};
+} // namespace mc
