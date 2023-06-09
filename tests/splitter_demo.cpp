1df4e4df691fc0bcb771be62c1a27a90512a9da2
FlareCoding <albert.slepak@gmail.com>
2023-06-08 23:24:35 -0400

    added demo tests for the splitter panel

---
+++ tests/splitter_demo.cpp
@@ -0,0 +1,20 @@
+#if defined(NDEBUG) && defined(_WIN32)
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif // !NDEBUG
+
+#include <window/ClassicWindow.h>
+#include <widgets/AllWidgets.h>
+#include <mcx/McxEngine.h>
+using namespace mc;
+
+int main() {
+    AppManager::registerApplication("appId-splitter-demo");
+
+    auto mainWindow = MakeRef<ClassicWindow>(880, 580, "SplitterPanel Demo");
+
+    auto rootPanel = mcx::McxEngine::parseUserWidgetFileAsContainer("splitterpanel_example1.mcx");
+    mainWindow->setRootWidget(rootPanel);
+
+    AppManager::startApplicationLoop();
+    return 0;
+}
