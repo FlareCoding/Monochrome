70a6c9fa6f76dbe2aa4bf05ec10357f90dbeace6
FlareCoding <albert.slepak@gmail.com>
2023-06-10 15:48:25 -0400

    intermediate update for testing

--- tests/splitter_demo.cpp
+++ tests/splitter_demo.cpp
@@ -7,6 +7,26 @@
 #include <mcx/McxEngine.h>
 using namespace mc;
 
+Shared<BaseContainerWidget> secondRootPanel;
+
+Shared<ClassicWindow> createSecondWindow(Shared<ClassicWindow> mainWindow) {
+    auto window = MakeRef<ClassicWindow>(580, 580, "Test Controller");
+
+    auto root = MakeRef<StackPanel>();
+    root->backgroundColor = Color::darkGray;
+    window->setRootWidget(root);
+
+    auto btn = MakeRef<Button>();
+    root->addChild(btn);
+    btn->on("clicked", [mainWindow](Shared<Event> e) {
+        secondRootPanel = mcx::McxEngine::parseUserWidgetFileAsContainer("splitterpanel_example2.mcx");
+        mainWindow->setRootWidget(secondRootPanel);
+        std::cout << "threadID: " << std::this_thread::get_id() << "\n";
+    });
+
+    return window;
+}
+
 int main() {
     AppManager::registerApplication("appId-splitter-demo");
 
@@ -14,6 +34,11 @@ int main() {
 
     auto rootPanel = mcx::McxEngine::parseUserWidgetFileAsContainer("splitterpanel_example1.mcx");
     mainWindow->setRootWidget(rootPanel);
+
+    //secondRootPanel = mcx::McxEngine::parseUserWidgetFileAsContainer("splitterpanel_example2.mcx");
+    std::cout << "main threadID: " << std::this_thread::get_id() << "\n";
+
+    auto secondWindow = createSecondWindow(mainWindow);
 
     AppManager::startApplicationLoop();
     return 0;
