70b4f0a3778768a540f900cc0b07388df715b868
FlareCoding <albert.slepak@gmail.com>
2023-06-13 00:00:09 -0400

    added some utiltiy classes to improve and increase abstraction

---
+++ mcstudio/GlobalResgitrationSystem.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Application ID
+#define MCSTUDIO_APPLICATION_ID "appId-mcstudio"
+
+// Custom color names
+#define MCSTUDIO_BG_COLOR_PRIMARY "mcstudioBgColorPrimary"
+
+// Event names
+// ...
+
+namespace mc::mcstudio {
+class GlobalRegistrationSystem {
+public:
+    // Registers every custom color name used in the monochrome studio design
+    static void registerMcStudioColorNames();
+};
+} // namespace mc::mcstudio
