793fee88e09d636c79bda4d323f063bc2d9132bb
FlareCoding <albert.slepak@gmail.com>
2023-06-17 16:27:15 -0400

    implemented extracting widget properties into an mcx node as string attributes

--- src/mcx/adapters/DropdownButtonMcxAdapter.h
+++ src/mcx/adapters/DropdownButtonMcxAdapter.h
@@ -9,6 +9,12 @@ public:
     /// @brief Creates and returns a native widget instance.
     Shared<BaseWidget> createWidgetInstance(Shared<McxNode>& mcxNode) override;
 
+    /// @brief Reads the properties of a given widget instance and
+    /// assigns them as attributes to a provided mcx node instance.
+    /// @param widget Widget instance from which to read the properties
+    /// @param mcxNode Mcx node into which to read the properties as attributes
+    virtual void extractProperties(Shared<BaseWidget>& widget, Shared<McxNode>& mcxNode) override;
+
     /// @brief Parses the mcx node and applies the provided
     /// mcx attributes to the appropriate widget instance members.
     /// @param widget Widget instance onto which the parsed attributes should be applied
