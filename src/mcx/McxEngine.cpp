76ca339f164d21e49d40711a7df5d6ea05c6cdff
Albert Slepak <41020147+FlareCoding@users.noreply.github.com>
2023-06-17 21:44:57 -0400

    Revert "added the ability to prefix every mcx file with a default root mcx directory path"
    This reverts commit c972d2a5c304a2260491b80502a484f5156640a5.

--- src/mcx/McxEngine.cpp
+++ src/mcx/McxEngine.cpp
@@ -27,29 +27,17 @@
 namespace mc::mcx {
     bool McxEngine::s_mcxEngineInitialized = false;
     std::map<std::string, Shared<McxParsingAdapter>> McxEngine::s_mcxAdapters;
-    std::string McxEngine::s_mcxRootDirectory = "";
 
     static Shared<BaseWidgetMcxAdapter> s_baseWidgetMcxAdapter = MakeRef<BaseWidgetMcxAdapter>();
 
-    void McxEngine::setRootMcxDirectory(const std::string& path) {
-        CORE_ASSERT(std::filesystem::is_directory(path), "Invalid mcx root directory provided");
-        s_mcxRootDirectory = path;
-
-        if (s_mcxRootDirectory.at(s_mcxRootDirectory.size() - 1) != '/') {
-            s_mcxRootDirectory += '/';
-        }
-    }
-
     Shared<ClassicWindow> McxEngine::parseWindowFile(const std::string& path) {
-        auto fullPath = s_mcxRootDirectory + path;
-
-        bool fileExists = std::filesystem::is_regular_file(fullPath);
+        bool fileExists = std::filesystem::is_regular_file(path);
         if (!fileExists) {
-            printf("'%s' could not be found\n", fullPath.c_str());
+            printf("'%s' could not be found\n", path.c_str());
             return nullptr;
         }
 
-        rapidxml::file<> xmlFile(fullPath.c_str());
+        rapidxml::file<> xmlFile(path.c_str());
         return parseWindowSource(xmlFile.data());
     }
 
@@ -99,15 +87,13 @@ namespace mc::mcx {
     }
 
     Shared<BaseWidget> McxEngine::parseUserWidgetFile(const std::string& path) {
-        auto fullPath = s_mcxRootDirectory + path;
-
-        bool fileExists = std::filesystem::is_regular_file(fullPath);
+        bool fileExists = std::filesystem::is_regular_file(path);
         if (!fileExists) {
-            printf("'%s' could not be found\n", fullPath.c_str());
+            printf("'%s' could not be found\n", path.c_str());
             return nullptr;
         }
 
-        rapidxml::file<> xmlFile(fullPath.c_str());
+        rapidxml::file<> xmlFile(path.c_str());
         return parseUserWidgetSource(xmlFile.data());
     }
 
