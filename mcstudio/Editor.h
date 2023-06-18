c5deb1023ea61ad4e3f95ad577202da5c531f469
FlareCoding <albert.slepak@gmail.com>
2023-06-17 22:50:05 -0400

    refactored the ToolboxWidget_OnClick function to contain comments and clearer logic for adding new widgets into the app view

--- mcstudio/Editor.h
+++ mcstudio/Editor.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <widgets/AllWidgets.h>
-#include "mcx/McxEngine.h"
+#include <mcx/McxEngine.h>
+#include <mcx/adapters/BaseWidgetMcxAdapter.h>
 
 namespace mc::mcstudio {
 class Editor {
@@ -11,10 +12,21 @@ public:
     void RootContainerSelection_OnClick(Shared<Event> e);
 
 private:
+    // Creates and returns new widget from a given widget name
+    Shared<BaseWidget> _spawnWidget(const std::string& widgetName);
+
     void _setSelectedWidget(Shared<BaseWidget> widget);
 
-    Shared<mcx::McxNode> d_limboNode;
+    // Node responsible for holding current selected widget's mcx information
+    Shared<mcx::McxNode> d_selectedWidgetNode;
+
+    // Adapter for basic widget properties
+    Shared<mcx::BaseWidgetMcxAdapter> d_baseWidgetAdapter;
+
+    // App's root container widget
     Shared<BaseContainerWidget> d_appRootContainer;
+
+    // Currently selected widget (either newly added or clicked on with a mouse)
     Shared<BaseWidget> d_selectedWidget;
 };
 } // namespace mc::mcstudio
