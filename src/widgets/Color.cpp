7065b0150ca7d5ccffd8ee76c20b217519ad6579
FlareCoding <albert.slepak@gmail.com>
2023-06-30 10:20:24 -0400

    made absolute colors such as red, green, and blue less bright and visually more appealing to look at

--- src/widgets/Color.cpp
+++ src/widgets/Color.cpp
@@ -20,10 +20,10 @@ namespace mc {
     Color Color::transparent    = Color(0, 0, 0, 0);
     Color Color::white          = Color(255, 255, 255);
     Color Color::black          = Color(0, 0, 0);
-    Color Color::red            = Color(255, 0, 0);
-    Color Color::green          = Color(0, 255, 0);
-    Color Color::blue           = Color(0, 0, 255);
-    Color Color::yellow         = Color(255, 255, 0);
+    Color Color::red            = Color(160, 0, 0);
+    Color Color::green          = Color(0, 160, 0);
+    Color Color::blue           = Color(0, 0, 160);
+    Color Color::yellow         = Color(160, 160, 0);
     Color Color::gray           = Color(100, 100, 100);
     Color Color::lightGray      = Color(160, 160, 160);
     Color Color::darkGray       = Color(60, 60, 60);
