7f72656e0336e4f394e68de2ec559696173caa5e
FlareCoding <albert.slepak@gmail.com>
2023-07-02 21:34:31 -0400

    added documentation comments to the TreeViewNode class

--- src/widgets/TreeView2.h
+++ src/widgets/TreeView2.h
@@ -10,35 +10,74 @@ public:
         const std::string& key = ""
     );
 
+    /// @brief Text displayed on the node in the TreeView
     PropertyObserver<std::string>   itemText;
+
+    /// @brief Unique string identifier belonging to the node in the TreeView
     PropertyObserver<std::string>   key;
+
+    /// @brief Determined whether or not the node is expanded and children
+    /// are shown in the TreeView. *Note* only works when the note has children.
     PropertyObserver<bool>          expanded;
 
-    //
-    // TO-DO: explain in docs that the below functions apply
-    //        only to DIRECT children on the current node level.
-    //
-
+    /// @brief Adds a node to the list of child nodes
+    /// @param node Node to add to the child nodes list
     void addChild(Shared<TreeViewNode> node);
+
+    /// @brief Creates and adds a node to the list of child nodes
+    /// @param itemText Text to be displayed on the child node
+    /// @param key Key to be assigned to the child node
     void addChild(const std::string& itemText, const std::string& key = "");
 
+    /// @brief Inserts a child node before another given child node
+    /// @param node Child node to be added
+    /// @param beforeChild The reference node before which to insert the child
     void insertChildBefore(Shared<TreeViewNode> node, Shared<TreeViewNode> beforeChild);
+
+    /// @brief Inserts a child node after another given child node
+    /// @param node Child node to be added
+    /// @param afterChild The reference node after which to insert the child
     void insertChildAfter(Shared<TreeViewNode> node, Shared<TreeViewNode> afterChild);
 
+    /// @brief Removes a given node from the children's list
+    /// @param node Reference to the node to be removed
+    /// @returns true if node was removed successfully, false otherwise
     bool removeChild(Shared<TreeViewNode> node);
+
+    /// @brief Removes a node with the given key from the children's list
+    /// @param key Key of the node to be removed
+    /// @returns true if node was removed successfully, false otherwise
     bool removeChild(const std::string& key);
 
+    /// @brief Checks if there is a direct child node with the given key
+    /// @param key Key of the child node
+    /// @returns true if there is a direct child node with the given key, false otherwise
     bool hasChildWithKey(const std::string& key);
+
+    /// @brief Assumes that the child node with the given key exists and attempts to return it
+    /// @param key Key of the child node
+    /// @returns Returns a reference to the child node with the given key.
+    /// If no such node exists, an exception will be thrown.
     Shared<TreeViewNode> getChildWithKey(const std::string& key);
+
+    /// @brief Checks if the child node with the given key exists and attempts to return it
+    /// @param key Key of the child node
+    /// @returns Returns a reference to the child node with
+    /// the given key if such node exists, nullptr otherwise.
     Shared<TreeViewNode> findChildByKey(const std::string& key);
 
+    /// @returns First child in the children's list if it exists, nullptr otherwise
     Shared<TreeViewNode> firstChild();
+
+    /// @returns Last child in the children's list if it exists, nullptr otherwise
     Shared<TreeViewNode> lastChild();
 
+    /// @returns Reference to the list of children nodes
     inline std::vector<Shared<TreeViewNode>>& getChildren() {
         return d_children;
     }
 
+    /// @returns Whether or not this node has child nodes
     inline bool hasChildren() const { return !d_children.empty(); }
 
 private:
@@ -47,7 +86,11 @@ private:
 
     void _invalidateKeyIndexMap();
 
+    /// @brief List of children nodes
     std::vector<Shared<TreeViewNode>> d_children;
+
+    /// @brief Map associating node keys with their
+    /// respective indices in the d_children list.
     std::map<std::string, size_t> d_nodeKeyIndexMap;
 };
 
