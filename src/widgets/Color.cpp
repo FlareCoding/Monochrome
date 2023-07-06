9b99703f472b41d67ebcd84342d2ddc20b4c368a
FlareCoding <albert.slepak@gmail.com>
2023-07-06 12:20:54 -0400

    defined the new colors with rgb values

--- src/widgets/Color.cpp
+++ src/widgets/Color.cpp
@@ -20,29 +20,29 @@ namespace mc {
     Color Color::transparent    = Color(0, 0, 0, 0);
     Color Color::white          = Color(255, 255, 255);
     Color Color::black          = Color(0, 0, 0);
-    Color Color::red            = Color(160, 0, 0);
+    Color Color::red            = Color(200, 0, 0);
     Color Color::green          = Color(0, 160, 0);
     Color Color::blue           = Color(0, 0, 160);
     Color Color::yellow         = Color(160, 160, 0);
     Color Color::gray           = Color(100, 100, 100);
     Color Color::lightGray      = Color(160, 160, 160);
     Color Color::darkGray       = Color(60, 60, 60);
-    Color Color::orange         = Color();
-    Color Color::cyan           = Color();
-    Color Color::purple         = Color();
-    Color Color::magenta        = Color();
-    Color Color::pink           = Color();
-    Color Color::salmon         = Color();
-    Color Color::brown          = Color();
-    Color Color::teal           = Color();
-    Color Color::maroon         = Color();
-    Color Color::lime           = Color();
-    Color Color::violet         = Color();
-    Color Color::olive          = Color();
-    Color Color::coral          = Color();
-    Color Color::chocolate      = Color();
-    Color Color::indigo         = Color();
-    Color Color::navy           = Color();
+    Color Color::orange         = Color(255, 140, 0);
+    Color Color::cyan           = Color(0, 180, 180);
+    Color Color::purple         = Color(128, 0, 128);
+    Color Color::magenta        = Color(255, 0, 255);
+    Color Color::pink           = Color(255, 192, 203);
+    Color Color::salmon         = Color(250, 128, 114);
+    Color Color::brown          = Color(139, 69, 19);
+    Color Color::teal           = Color(0, 128, 128);
+    Color Color::maroon         = Color(128, 0, 0);
+    Color Color::lime           = Color(0, 255, 0);
+    Color Color::violet         = Color(138, 43, 226);
+    Color Color::olive          = Color(128, 128, 0);
+    Color Color::coral          = Color(255, 127, 80);
+    Color Color::chocolate      = Color(210, 105, 30);
+    Color Color::indigo         = Color(75, 0, 130);
+    Color Color::navy           = Color(0, 0, 128);
 
     void registerCustomColor(const std::string& name, const Color& color) {
         CORE_ASSERT(s_customColorMap.find(name) == s_customColorMap.end(),
