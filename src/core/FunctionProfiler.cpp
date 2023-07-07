81dbe8e73dba97f21e62f8f07eaef763b160543d
FlareCoding <albert.slepak@gmail.com>
2023-07-06 21:47:31 -0400

    fixed cpplint errors

--- src/core/FunctionProfiler.cpp
+++ src/core/FunctionProfiler.cpp
@@ -1,59 +1,61 @@
 #include "FunctionProfiler.h"
 
 namespace mc::debug {
-	FunctionProfiler::FunctionProfiler(const char* fnName) {
-		d_running = true;
-		d_fnName = fnName;
+    FunctionProfiler::FunctionProfiler(const char* fnName) {
+        d_running = true;
+        d_fnName = fnName;
 
-		d_startTimestamp = std::chrono::high_resolution_clock::now();
-	}
+        d_startTimestamp = std::chrono::high_resolution_clock::now();
+    }
 
-	FunctionProfiler::~FunctionProfiler() {
-		if (d_running) {
-			endProfiling();
-		}
-	}
+    FunctionProfiler::~FunctionProfiler() {
+        if (d_running) {
+            endProfiling();
+        }
+    }
 
-	void FunctionProfiler::endProfiling() {
-		auto endTimepoint = std::chrono::high_resolution_clock::now();
+    void FunctionProfiler::endProfiling() {
+        auto endTimepoint = std::chrono::high_resolution_clock::now();
 
-		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(
-			d_startTimestamp
-		).time_since_epoch().count();
+        int64_t start = std::chrono::time_point_cast<std::chrono::microseconds>(
+            d_startTimestamp
+        ).time_since_epoch().count();
 
-		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(
-			endTimepoint
-		).time_since_epoch().count();
+        int64_t end = std::chrono::time_point_cast<std::chrono::microseconds>(
+            endTimepoint
+        ).time_since_epoch().count();
 
-		d_running = false;
-		
-		uint64_t threadId = std::hash<std::thread::id>{}(std::this_thread::get_id());
-		uint64_t duration = static_cast<uint64_t>(end - start);
+        d_running = false;
 
-		ProfilerRecord record;
-		record.functionName = d_fnName;
-		record.durationInMicroseconds = duration;
-		record.threadId = threadId;
+        uint64_t threadId = std::hash<std::thread::id>{}(std::this_thread::get_id());
+        uint64_t duration = static_cast<uint64_t>(end - start);
 
-		GlobalFunctionProfilerRegistry::get().recordProfilerSession(record);
-	}
+        ProfilerRecord record;
+        record.functionName = d_fnName;
+        record.durationInMicroseconds = duration;
+        record.threadId = threadId;
 
-	GlobalFunctionProfilerRegistry& GlobalFunctionProfilerRegistry::get() {
-		static auto s_globalRegistry = MakeRef<GlobalFunctionProfilerRegistry>();
-		return *s_globalRegistry;
-	}
+        GlobalFunctionProfilerRegistry::get().recordProfilerSession(record);
+    }
 
-	void GlobalFunctionProfilerRegistry::recordProfilerSession(ProfilerRecord& record) {
-		d_profilerRecords[record.functionName] = record;
-	}
+    GlobalFunctionProfilerRegistry& GlobalFunctionProfilerRegistry::get() {
+        static auto s_globalRegistry = MakeRef<GlobalFunctionProfilerRegistry>();
+        return *s_globalRegistry;
+    }
 
-	ProfilerRecord& GlobalFunctionProfilerRegistry::getProfilerSessionRecord(const std::string& fnName) {
-		static ProfilerRecord s_limboRecord = ProfilerRecord("Unknown", 0, 0);
+    void GlobalFunctionProfilerRegistry::recordProfilerSession(ProfilerRecord& record) {
+        d_profilerRecords[record.functionName] = record;
+    }
 
-		if (d_profilerRecords.find(fnName) != d_profilerRecords.end()) {
-			return d_profilerRecords[fnName];
-		}
+    ProfilerRecord& GlobalFunctionProfilerRegistry::getProfilerSessionRecord(
+        const std::string& fnName
+    ) {
+        static ProfilerRecord s_limboRecord = ProfilerRecord("Unknown", 0, 0);
 
-		return s_limboRecord;
-	}
+        if (d_profilerRecords.find(fnName) != d_profilerRecords.end()) {
+            return d_profilerRecords[fnName];
+        }
+
+        return s_limboRecord;
+    }
 } // namespace mc::debug
