7eca26a14846eeba031336bb055cc5b932c05963
FlareCoding <albert.slepak@gmail.com>
2023-07-09 18:56:38 -0400

    implemented a custom boolean property field for 'visible' base property

--- mcstudio/controllers/WidgetPropertiesPanelController.cpp
+++ mcstudio/controllers/WidgetPropertiesPanelController.cpp
@@ -28,7 +28,14 @@ namespace mc::mcstudio {
             /*auto propertyEntry = _createPropertyEntry(prop, true, widget, mcxNode, baseAdapter, customAdapter);*/
             auto propValue = widgetNode->getAttribute(prop);
 
-            auto propertyField = _createRegularPropertyTextField(prop, propValue, true);
+            Shared<BaseWidget> propertyField;
+            if (propValue == "true" || propValue == "false") {
+                propertyField = _createBooleanPropertyField(prop, propValue, true);
+            }
+            else {
+                propertyField = _createRegularPropertyTextField(prop, propValue, true);
+            }
+
             propertiesListPanel->addChildOffline(propertyField);
         }
 
@@ -93,6 +100,113 @@ namespace mc::mcstudio {
             });
         });
         container->addChildOffline(entry);
+
+        return container;
+    }
+
+    Shared<BaseWidget> WidgetPropertiesPanelController::_createBooleanPropertyField(
+        const std::string& propName,
+        const std::string& propValue,
+        bool isBasicProperty
+    ) {
+        static auto dimmedColor = Color(53, 54, 59);
+        bool propBoolVal = propValue == "true";
+
+        auto container = MakeRef<StackPanel>();
+        container->orientation = Horizontal;
+        container->backgroundColor = Color::transparent;
+        container->marginBottom = 4;
+
+        auto propertyNameLabel = MakeRef<Label>();
+        propertyNameLabel->text = propName;
+        propertyNameLabel->fixedWidth = 110;
+        propertyNameLabel->fixedHeight = 20;
+        propertyNameLabel->marginLeft = 10;
+        propertyNameLabel->alignment = "left";
+        container->addChildOffline(propertyNameLabel);
+
+        auto switchStackPanel = MakeRef<StackPanel>();
+        switchStackPanel->orientation = Horizontal;
+        switchStackPanel->backgroundColor = Color::transparent;
+        switchStackPanel->fixedHeight = 20;
+
+        auto falseButton = MakeRef<Button>();
+        falseButton->fixedHeight = 16;
+        falseButton->verticalAlignment = VACenter;
+        falseButton->label->text = "false";
+        falseButton->label->color = Color::white;
+        falseButton->backgroundColor = Color(100, 0, 0);
+        falseButton->borderColor = Color::transparent;
+        falseButton->cornerRadius = 0;
+        switchStackPanel->addChildOffline(falseButton);
+
+        auto trueButton = MakeRef<Button>();
+        trueButton->fixedHeight = 16;
+        trueButton->verticalAlignment = VACenter;
+        trueButton->label->text = "true";
+        trueButton->label->color = Color::white;
+        trueButton->backgroundColor = Color(0, 100, 0);
+        trueButton->borderColor = Color::transparent;
+        trueButton->cornerRadius = 0;
+        switchStackPanel->addChildOffline(trueButton);
+
+        if (propBoolVal) {
+            falseButton->backgroundColor = dimmedColor;
+        } else {
+            trueButton->backgroundColor = dimmedColor;
+        }
+
+        falseButton->on("clicked", [this, isBasicProperty](Shared<Event> e) {
+            // Get the property name through the parent's sibling label
+            auto target = static_cast<Button*>(e->target);
+            auto parentContainer = static_cast<StackPanel*>(target->getParent()->getParent());
+            auto propertyNameLabel =
+                std::static_pointer_cast<Label>(parentContainer->getChild(0));
+
+            auto propName = propertyNameLabel->text.get();
+
+            // Highlight the "false" button
+            target->backgroundColor = Color(100, 0, 0);
+
+            // Get the "true" button and dim it
+            auto trueButton = std::static_pointer_cast<Button>(
+                static_cast<StackPanel*>(target->getParent())->getChild(1)
+            );
+            trueButton->backgroundColor = dimmedColor;
+
+            fireEvent("widgetPropertyModified", {
+                { "propName", propName },
+                { "propValue", std::string("false") },
+                { "isBasicProp", isBasicProperty },
+            });
+        });
+
+        trueButton->on("clicked", [this, isBasicProperty](Shared<Event> e) {
+            // Get the property name through the parent's sibling label
+            auto target = static_cast<Button*>(e->target);
+            auto parentContainer = static_cast<StackPanel*>(target->getParent()->getParent());
+            auto propertyNameLabel =
+                std::static_pointer_cast<Label>(parentContainer->getChild(0));
+
+            auto propName = propertyNameLabel->text.get();
+
+            // Highlight the "true" button
+            target->backgroundColor = Color(0, 100, 0); 
+
+            // Get the "false" button and dim it
+            auto falseButton = std::static_pointer_cast<Button>(
+                static_cast<StackPanel*>(target->getParent())->getChild(0)
+            );
+            falseButton->backgroundColor = dimmedColor;
+
+            fireEvent("widgetPropertyModified", {
+                { "propName", propName },
+                { "propValue", std::string("true") },
+                { "isBasicProp", isBasicProperty },
+            });
+        });
+
+        container->addChildOffline(switchStackPanel);
 
         return container;
     }
