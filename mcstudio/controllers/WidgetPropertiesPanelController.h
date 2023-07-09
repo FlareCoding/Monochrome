8513a51b4b4b52b1c83e1cbec37a99094ecc62e4
FlareCoding <albert.slepak@gmail.com>
2023-07-08 22:20:09 -0400

    moved the widget property editing logic into its separate controller class

--- mcstudio/controllers/WidgetPropertiesPanelController.h
+++ mcstudio/controllers/WidgetPropertiesPanelController.h
@@ -1,12 +1,46 @@
 #pragma once
 #include <widgets/AllWidgets.h>
+#include <mcx/adapters/BaseWidgetMcxAdapter.h>
 
 namespace mc::mcstudio {
-class WidgetPropertiesPanelController {
+class WidgetPropertyModifiedEvent : public Event {
 public:
-    WidgetPropertiesPanelController();
+    inline std::string getPropName() {
+        return get<std::string>("propName");
+    }
+
+    inline std::string getPropValue() {
+        return get<std::string>("propValue");
+    }
+
+    inline bool isBasicProperty() {
+        return get<bool>("isBasicProp");
+    }
+};
+
+class WidgetPropertiesPanelController : public EventEmitter {
+public:
+    WidgetPropertiesPanelController(Shared<mcx::BaseWidgetMcxAdapter> baseWidgetAdapter);
+
+    // Removes all property entries from the widget properties panel
+    void clear();
+
+    // Fills the properties panel with the provided widget's properties
+    void displayWidgetProperties(
+        Shared<mcx::McxNode> widgetNode,
+        Shared<mcx::McxParsingAdapter> widgetAdapter
+    );
 
 private:
     Shared<StackPanel> d_propertiesPanel;
+    Shared<mcx::BaseWidgetMcxAdapter> d_baseWidgetAdapter;
+
+    // Creates a widget container consisting of a label and an entry
+    // which upon modification will modify the selected widget's property.
+    Shared<BaseWidget> _createRegularPropertyTextField(
+        const std::string& propName,
+        const std::string& propValue,
+        bool isBasicProperty
+    );
 };
 } // namespace mc::mcstudio
