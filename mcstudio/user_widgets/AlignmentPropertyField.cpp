b9d6cf8b4aa1788188aabd28cd1e81d76dd4bc14
FlareCoding <albert.slepak@gmail.com>
2023-07-10 20:00:38 -0400

    handmade custom alignment icons for base widget properties

--- mcstudio/user_widgets/AlignmentPropertyField.cpp
+++ mcstudio/user_widgets/AlignmentPropertyField.cpp
@@ -27,23 +27,53 @@ namespace mc::mcstudio {
         propertyNameLabel->alignment = "left";
         addChildOffline(propertyNameLabel);
 
-        auto leftAlignButton = _createAlignmentImgButton("left", 1);
-        addChildOffline(leftAlignButton);
+        Shared<Button>  alignControlButton1,
+                        alignControlButton2,
+                        alignControlButton3,
+                        alignControlButton4;
 
-        auto centerAlignButton = _createAlignmentImgButton("center", 2);
-        addChildOffline(centerAlignButton);
+        if (d_propName == "horizontalAlignment") {
+            alignControlButton1 = _createAlignmentImgButton("left", 1);
+            alignControlButton2 = _createAlignmentImgButton("center", 2);
+            alignControlButton3 = _createAlignmentImgButton("right", 3);
+            alignControlButton4 = _createAlignmentImgButton("fill", 4);
 
-        auto rightAlignButton = _createAlignmentImgButton("right", 3);
-        addChildOffline(rightAlignButton);
+        } else if (d_propName == "verticalAlignment") {
+            alignControlButton1 = _createAlignmentImgButton("top", 1);
+            alignControlButton2 = _createAlignmentImgButton("center", 2);
+            alignControlButton3 = _createAlignmentImgButton("bottom", 3);
+            alignControlButton4 = _createAlignmentImgButton("fill", 4);
+        }
 
-        auto fillAlignButton = _createAlignmentImgButton("fill", 4);
-        addChildOffline(fillAlignButton);
+        addChildOffline(alignControlButton1);
+        addChildOffline(alignControlButton2);
+        addChildOffline(alignControlButton3);
+        addChildOffline(alignControlButton4);
+
+        if (d_propValue == "left" || d_propValue == "top") {
+            alignControlButton1->backgroundColor = d_selectedControlButtonColor;
+
+        } else if (d_propValue == "center") {
+            alignControlButton2->backgroundColor = d_selectedControlButtonColor;
+
+        } else if (d_propValue == "right" || d_propValue == "bottom") {
+            alignControlButton3->backgroundColor = d_selectedControlButtonColor;
+        } else {
+            alignControlButton4->backgroundColor = d_selectedControlButtonColor;
+        }
     }
 
     Shared<Button> AlignmentPropertyField::_createAlignmentImgButton(
         const std::string& alignment,
         uint32_t orderIdx
     ) {
+        std::string imgPath = "";
+        if (d_propName == "horizontalAlignment") {
+            imgPath = "icons/halign-" + alignment + ".png";
+        } else if (d_propName == "verticalAlignment") {
+            imgPath = "icons/valign-" + alignment + ".png";
+        }
+
         auto button = MakeRef<Button>();
         button->zIndex = orderIdx;
         button->borderColor = Color::transparent;
@@ -54,14 +84,19 @@ namespace mc::mcstudio {
         button->fixedWidth = 24;
         button->fixedHeight = 24;
         button->imagePlacement = Cover;
-        button->setImage(Image::loadFromFile("icons/align-" + alignment + ".png"));
+        button->setImage(Image::loadFromFile(imgPath));
         button->on("clicked", &AlignmentPropertyField::_alignmentControlButton_OnClick, this);
 
         return button;
     }
 
     void AlignmentPropertyField::_alignmentControlButton_OnClick(Shared<Event> e) {
+        _dimAllControlButtons();
+
         auto orderIdx = e->target->zIndex.get();
+
+        auto targetBtn = std::static_pointer_cast<Button>(getChild(orderIdx));
+        targetBtn->backgroundColor = d_selectedControlButtonColor;
 
         if (d_propName == "horizontalAlignment") {
             switch (orderIdx) {
@@ -109,5 +144,12 @@ namespace mc::mcstudio {
             { "propName", d_propName },
             { "propValue", d_propValue },
         });
+    }
+
+    void AlignmentPropertyField::_dimAllControlButtons() {
+        for (uint64_t i = 1; i < 5; ++i) {
+            auto btn = std::static_pointer_cast<Button>(getChild(i));
+            btn->backgroundColor = getCustomColor("mcstudioBgColorPrimary");
+        }
     }
 } // namespace mc::mcstudio
