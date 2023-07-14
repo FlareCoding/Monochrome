98ab51fcc46c890b47284ddf0f3b97fd45c4a776
FlareCoding <albert.slepak@gmail.com>
2023-07-13 21:56:58 -0400

    temporarily removed the overlay edit frame drawing from mcstudio due to canvas API rework

--- mcstudio/controllers/EditorCanvasController.cpp
+++ mcstudio/controllers/EditorCanvasController.cpp
@@ -2,36 +2,43 @@
 
 namespace mc::mcstudio {
     EditorCanvasController::EditorCanvasController(Shared<Canvas2D> overlayCanvas) {
-        d_canvas = overlayCanvas;
+        //d_canvas = overlayCanvas;
     }
 
     void EditorCanvasController::clearCanvas() {
-        d_canvas->clear();
+        //d_canvas->clear();
     }
 
     void EditorCanvasController::drawWidgetEditFrame(Shared<BaseWidget> widget) {
-        auto widgetAbsPosition = widget->getPositionInWindow();
-        auto widgetSize = widget->getComputedSize();
+        //auto widgetAbsPosition = widget->getPositionInWindow();
+        //auto widgetSize = widget->getComputedSize();
 
-        // Draw the rectangular frame around the widget
-        d_canvas->strokeRectangle(
-            widgetAbsPosition,
-            widgetSize,
-            widgetEditFrameColor
-        );
+        //// Draw the rectangular frame around the widget
+        //d_canvas->strokeRectangle(
+        //    widgetAbsPosition,
+        //    widgetSize,
+        //    widgetEditFrameColor
+        //);
 
-        // Draw the widget's name/type above the editor frame
-        widgetAbsPosition.y -= 20;
-        widgetSize.height = 20;
-        d_canvas->fillText(
-            widgetAbsPosition,
-            widgetSize,
-            widgetEditFrameColor,
-            widget->getWidgetName(),
-            "Verdana",
-            14,
-            "normal",
-            "left"
-        );
+        //// Draw the widget's name/type above the editor frame
+        //widgetAbsPosition.y -= 20;
+        //widgetSize.height = 20;
+        //d_canvas->fillText(
+        //    widgetAbsPosition,
+        //    widgetSize,
+        //    widgetEditFrameColor,
+        //    widget->getWidgetName(),
+        //    "Verdana",
+        //    14,
+        //    "normal",S
+        //    "left"
+        //);
+
+        /*
+        * 
+        *  THE CANVAS API IS CURRENTLY IN DEVELOPMENT AND SHOULD NOT BE USED.
+        *  THE ISSUE IS PLANNED AND EXPECTED TO BE ADDRESSED IN COMING UDPATES.
+        * 
+        */
     }
 } // namespace mc::mcstudio
